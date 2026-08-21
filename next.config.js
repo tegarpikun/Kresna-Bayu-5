baf90aecbff5a381a11a56495297e2edc06c31e7
tegarpikun <tegarpikun@gmail.com>
2026-08-21 13:47:58 +0700

    Add caching headers for lottie assets

--- next.config.js
+++ next.config.js
@@ -50,6 +50,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/lottie/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
     ];
   },
 };
