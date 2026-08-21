2e01b652a1c1d4e8dd89137e1303340c67ed0950
tegarpikun <tegarpikun@gmail.com>
2026-08-21 18:52:16 +0700

    Add Cache-Control headers for brand path

--- next.config.js
+++ next.config.js
@@ -51,6 +51,15 @@ const nextConfig = {
         ],
       },
       {
+        source: '/brand/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
         source: '/lottie/:path*',
         headers: [
           {
