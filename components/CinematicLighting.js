5345ad39c275f0ee711964b877766d6c041b0795
tegarpikun <tegarpikun@gmail.com>
2026-08-21 18:49:36 +0700

    Adjust lighting intensities in CinematicLighting component

--- components/CinematicLighting.js
+++ components/CinematicLighting.js
@@ -3,25 +3,25 @@
 export default function CinematicLighting() {
   return (
     <>
-      <ambientLight intensity={0.14} color="#1a1a2e" />
+      <ambientLight intensity={0.42} color="#3a3a52" />
 
       {/* Key light - hangat (amber), dari kanan atas */}
       <directionalLight
         position={[8, 6, 4]}
-        intensity={1.7}
+        intensity={2.1}
         color="#d4a574"
       />
 
       {/* Fill light - dingin (teal), dari kiri bawah */}
       <directionalLight
         position={[-6, -3, 3]}
-        intensity={1}
+        intensity={1.3}
         color="#2c7a7b"
       />
 
       {/* Rim light tipis dari belakang objek - satu titik cukup untuk
           kesan siluet, tanpa menambah beban shader terlalu banyak. */}
-      <pointLight position={[0, 2, -14]} intensity={0.7} color="#ffffff" distance={30} />
+      <pointLight position={[0, 2, -14]} intensity={0.9} color="#ffffff" distance={30} />
     </>
   );
 }
